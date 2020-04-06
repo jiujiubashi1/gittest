2e24a35878ed070805049ed60e91ea695523a545
周小龙 <303173008@qq.com>
2020-04-06 23:00:00 +0800

    首次提交

---
+++ src/main/java/com/qingniao/Gittest.java
@@ -0,0 +1,13 @@
+package com.qingniao;
+
+/**
+ * @author 周小龙
+ * @version 1.00
+ * @time 2020/4/6 22:55
+ * @description 测试git
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
